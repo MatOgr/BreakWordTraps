eaa49d9aa0f6b3572ef92cb8f88b83227c4db22a
Mateusz Ogrodowczyk <ogrodowczykmat@gmail.com>
2024-09-29 09:19:34 +0200

    Add pose estimation drawer class

--- break_word_traps/tools/pose/drawing.py
+++ break_word_traps/tools/pose/drawing.py
@@ -1,36 +1,45 @@
 import cv2
 import mediapipe as mp
 
-mp_pose = mp.solutions.pose
-drawing_utils = mp.solutions.drawing_utils
-drawing_styles = mp.solutions.drawing_styles
 
+class PoseDrawer:
+    def __init__(self):
+        self.pose = mp.solutions.pose
+        self.pose_connections = self.pose.POSE_CONNECTIONS
+        self.drawing_styles = mp.solutions.drawing_styles
+        self.drawing_utils = mp.solutions.drawing_utils
 
-def draw_pose(pose, image):
-    drawing = drawing_utils.draw_landmarks(
-        image,
-        pose.pose_landmarks,
-        mp_pose.POSE_CONNECTIONS,
-        landmark_drawing_spec=drawing_styles.get_default_pose_landmarks_style(),
-    )
-    return drawing
+    def draw_pose(self, pose_estimation, image):
+        drawing = self.drawing_utils.draw_landmarks(
+            image,
+            pose_estimation.pose_landmarks,
+            self.pose_connections,
+            landmark_drawing_spec=self.drawing_styles.get_default_pose_landmarks_style(),
+        )
+        return drawing
 
+    def process_frame(self, pose, image):
+        pose_estimation = pose.process(image)
+        image_with_pose = self.draw_pose(pose_estimation, image)
+        return image_with_pose
 
-def process_frame(image):
-    pose_estimation = mp_pose.process(image)
-    image_with_pose = draw_pose(pose_estimation, image)
-    return image_with_pose
-
-
-def process_video(video_path):
-    cap = cv2.VideoCapture(video_path)
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
-        frame_with_pose = process_frame(frame)
-        # cv2.imshow("frame", frame_with_pose)
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
-    cap.release()
-    # cv2.destroyAllWindows()
+    def process_video(self, video_file):
+        # TODO: Add support for video processing
+        with cv2.VideoCapture(video_file) as cap:
+            with self.pose.Pose(
+                static_image_mode=True,
+                model_complexity=0,
+                enable_segmentation=False,
+                min_detection_confidence=0.7,
+            ) as pose:
+                success, frame = cap.read()
+                frames = []
+                while success:
+                    frame_with_pose = self.process_frame(pose, frame)
+                    frames.append(frame_with_pose)
+                    # cv2.imshow("frame", frame_with_pose)
+                    if cv2.waitKey(1) & 0xFF == ord("q"):
+                        break
+                    success, frame = cap.read()
+        return frames
+        # cv2.destroyAllWindows()
